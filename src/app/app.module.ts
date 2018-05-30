1bcb582d162d02ab52b918c48a953b8fdc37cec0
Felino7727 <antontsev.nikita@gmail.com>
2018-05-30 11:05:42 +0300

    30052018 11.04

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListNavigationComponent } from './list-navigation/list-navigation.component';
+import { ListBuyComponent } from './list-navigation/list-buy/list-buy.component';
+import { ListWonComponent } from './list-navigation/list-won/list-won.component';
+import { ListHomeComponent } from './list-navigation/list-home/list-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListNavigationComponent,
+    ListBuyComponent,
+    ListWonComponent,
+    ListHomeComponent
   ],
   imports: [
     BrowserModule
