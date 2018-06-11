2e73a2cbb7190ca77f766fef2138383edf60afa1
Felino7727 <antontsev.nikita@gmail.com>
2018-06-11 15:16:15 +0300

    11062018

--- src/app/share/models/tickets.model.ts
+++ src/app/share/models/tickets.model.ts
@@ -3,6 +3,8 @@ export class Tickets {
     public email: string,
     public payeer: string,
     public win: boolean = false,
+    public date_buy: any = Date.now(),
+    public date_game: any = Date.now(),
     public id?: number
   ) {
   }
