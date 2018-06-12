c6ebf83385d359d0bc3c2300fce121ca68f48302
Felino7727 <antontsev.nikita@gmail.com>
2018-06-12 16:43:00 +0300

    12062018

--- src/app/list-navigation/list-home/list-home.component.ts
+++ src/app/list-navigation/list-home/list-home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {TicketsService} from '../../share/service/tickets.service';
+import {Winnumber} from '../../share/models/winnumber.model';
+import {Tickets} from '../../share/models/tickets.model';
+import {forEach} from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'anv-list-home',
@@ -7,7 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListHomeComponent implements OnInit {
 
-  constructor() {  }
+  constructor(private winnumberService:TicketsService){}
+
+  a:number;
+  ticket:Tickets[];
+  buyticket:Winnumber[];
+  onWinNumber() {
+    this.a=Math.floor(Math.random()*(5-3)+3);
+    this.winnumberService.getWinTickets().subscribe(ticket => {
+      this.ticket = ticket.filter(c=>c.id%this.a==0);
+    });
+    
+    console.log(this.a);
+    console.log(this.ticket);
+  }
+
   ngOnInit() {  }
 
 }
